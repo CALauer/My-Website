a365b280ab65e49142763d21204437d3ecd58143
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2020-12-19 04:23:08 -0500

    Code Clean Up
    Adjusted Page layout, cleaned up javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,43 +1,68 @@
-$('#hero-arrow-btn-mobile, #hero-arrow-btn, #projects-link, #cta-my-work, #mobile-link-projects').click(function() {
-    console.log("clicked");
-	$([ document.documentElement, document.body ]).animate(
-		{
-			scrollTop: $('#projects').offset().top
-		},
-		800
-	);
-});
-$('#home-link, #home, #mobile-link-top').click(function() {
-    console.log("clicked");
-	$([ document.documentElement, document.body ]).animate(
-		{
-			scrollTop: $('#top').offset().top
-		},
-		800
-	);
-});
-$('#designs-link, #mobile-link-designs').click(function() {
-    console.log("clicked");
-	$([ document.documentElement, document.body ]).animate(
-		{
-			scrollTop: ( $('#designs').offset().top - 50 )
-		},
-		800
-	);
-});
 
-// $('#live-demo-no').click(function() {
-// 	alert("This website is currently unavilable. I will be uploading this to the server ina few days. Please checkback later. Sorry.");
-// })
+$("a").click(function() {
+	console.log(this.id);
+	let id = this.id;
+	let dropDownMenu = $('.dropdown-menu');
 
-$('#mobile-menu-btn, .mobile-menu-display').click('click', function() {
-	let x = $('.mobile-menu-display');
-	if (x.css('top') !== '50px') {
-		x.animate({top: 50 });
+	if (id == ('home') || id ==('home-link') ||  id ==('mobile-link-top')) {
+		section = "top";
+		scrollTo(section); // Passes the section to scroll to
+		checkMobileMenu();
+		} 
+		if (id == ('dropdown-project1')) {
+				section = "project1";
+				dropDownMenu.fadeToggle(200).delay( 100 ).css({display: 'grid'});
+				scrollTo(section);
+			}
+			if (id == ('dropdown-project3')) {
+				section = "project3";
+				dropDownMenu.fadeToggle(200).delay( 100 ).css({display: 'grid'});
+				scrollTo(section);
+			}
+				if (id == ('cta-my-work') || id ==('mobile-link-projects')) {
+					section = "projects";
+					scrollTo(section);
+					checkMobileMenu();
+				}
+					if (id == ('designs-link') || id ==('mobile-link-designs')) {
+						section = "designs";
+						scrollTo(section);
+						checkMobileMenu();
+					}
+						if (id == ('aboutme-link') || id ==('mobile-link-aboutme') || id ==('hero-arrow-btn-mobile')|| id ==('hero-arrow-btn')) {
+							section = "aboutme";
+							scrollTo(section);
+							checkMobileMenu();
+						}
+							if (id == ('projects-link')) {
+								dropDownMenu.fadeToggle(200).delay( 100 ).css({display: 'grid'});
+							}
+								if (id == ('mobile-menu-btn') || id == ('mobile-menu-display')) {
+									checkMobileMenu();
+								}
+					else {
+						console.log("false");
+
+					}
+				});
+
+// scroll function {
+	function scrollTo(section) {
+		var section = section;
+		$([ document.documentElement, document.body ]).animate(
+			{
+				scrollTop: ($('#' + section).offset().top - 60 )
+			},
+			800
+		);
+	} 
+// Checks if mobile mobile menu
+function checkMobileMenu() {
+	let mobileMenu = $('.mobile-menu-display')
+	if (mobileMenu.css('top') !== '50px') {
+		mobileMenu.animate({top: 50 });
 		}
 		else {
-			x.animate({top: '-100%', height: 'auto'});
-	} 
-	});
-
-
+			mobileMenu.animate({top: '-100%', height: 'auto'});
+}
+}
