26ffbf2825e157b53ef08d3527919be6db676863
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2021-01-17 14:20:20 -0500

    Update
    Added new images, added NodeJS app to viabull project link

--- scripts/script.js
+++ scripts/script.js
@@ -3,46 +3,50 @@ $("a").click(function() {
 	let id = this.id;
 	let dropDownMenu = $('.dropdown-menu');
 
-	if (id == ('home') || id ==('home-link') ||  id ==('mobile-link-top')) {
+	if (id == ('home') || id ==('home-link')) {
 		section = "top";
 		scrollTo(section); // Passes the section to scroll to
-		checkMobileMenu();
 		} 
-		if (id == ('dropdown-project1')) {
-				section = "project1";
-				dropDownMenu.fadeToggle(200).delay( 100 ).css({display: 'grid'});
-				scrollTo(section);
-			}
-			if (id == ('dropdown-project3')) {
-				section = "project3";
-				dropDownMenu.fadeToggle(200).delay( 100 ).css({display: 'grid'});
-				scrollTo(section);
-			}
-				if (id == ('cta-my-work') || id ==('mobile-link-projects')) {
-					section = "projects";
+		if (id ==('mobile-link-top')) {
+			section = "top";
+			scrollTo(section); // Passes the section to scroll to
+			checkMobileMenu();
+			} 
+			if (id == ('dropdown-project1')) {
+					section = "project1";
+					dropDownMenu.fadeToggle(200).delay( 100 ).css({display: 'grid'});
 					scrollTo(section);
-					checkMobileMenu();
 				}
-					if (id == ('designs-link') || id ==('mobile-link-designs')) {
-						section = "designs";
+				if (id == ('dropdown-project3')) {
+					section = "project3";
+					dropDownMenu.fadeToggle(200).delay( 100 ).css({display: 'grid'});
+					scrollTo(section);
+				}
+					if (id == ('cta-my-work') || id ==('mobile-link-projects')) {
+						section = "projects";
 						scrollTo(section);
 						checkMobileMenu();
 					}
-						if (id == ('aboutme-link') || id ==('mobile-link-aboutme') || id ==('hero-arrow-btn-mobile')|| id ==('hero-arrow-btn')) {
-							section = "aboutme";
+						if (id == ('designs-link') || id ==('mobile-link-designs')) {
+							section = "designs";
 							scrollTo(section);
 							checkMobileMenu();
 						}
-							if (id == ('projects-link')) {
-								dropDownMenu.fadeToggle(200).delay( 100 ).css({display: 'grid'});
+							if (id == ('aboutme-link') || id ==('mobile-link-aboutme') || id ==('hero-arrow-btn-mobile') || id ==('hero-arrow-btn')) {
+								section = "aboutme";
+								scrollTo(section);
+								console.log("clicked");
+								checkMobileMenu();
 							}
-								if (id == ('mobile-menu-btn') || id == ('mobile-menu-display')) {
-									checkMobileMenu();
+								if (id == ('projects-link')) {
+									dropDownMenu.fadeToggle(200).delay( 100 ).css({display: 'grid'});
+									$(dropDownMenu).mouseleave( function() {
+										$(dropDownMenu).fadeOut(200);
+									});
 								}
-					else {
-						
-
-					}
+									if (id == ('mobile-menu-btn') || id == ('mobile-menu-display')) {
+										checkMobileMenu();
+									}
 				});
 
 // scroll function {
