00e76aea5261baf612d74efcb046ef839afe149b
Chris Lauer <56165582+CALauer@users.noreply.github.com>
2021-04-01 10:02:09 -0400

    Minor Update
    Deleted unnecessary photos and files. Added a hover effect to project images. Added SASS files to incorporate browser standards. Updated images for performance improvements.  Updated PSD for easier updates.

--- scripts/script.js
+++ scripts/script.js
@@ -1,71 +1,69 @@
-
 $("a").click(function() {
-	let id = this.id;
-	let dropDownMenu = $('.dropdown-menu');
+    let id = this.id;
+    let dropDownMenu = $('.dropdown-menu');
 
-	if (id == ('home') || id ==('home-link')) {
-		section = "top";
-		scrollTo(section); // Passes the section to scroll to
-		} 
-		if (id ==('mobile-link-top')) {
-			section = "top";
-			scrollTo(section); // Passes the section to scroll to
-			checkMobileMenu();
-			} 
-			if (id == ('dropdown-project1')) {
-					section = "project1";
-					dropDownMenu.fadeToggle(200).delay( 100 ).css({display: 'grid'});
-					scrollTo(section);
-				}
-				if (id == ('dropdown-project3')) {
-					section = "project3";
-					dropDownMenu.fadeToggle(200).delay( 100 ).css({display: 'grid'});
-					scrollTo(section);
-				}
-					if (id == ('cta-my-work') || id ==('mobile-link-projects')) {
-						section = "projects";
-						scrollTo(section);
-						checkMobileMenu();
-					}
-						if (id == ('designs-link') || id ==('mobile-link-designs')) {
-							section = "designs";
-							scrollTo(section);
-							checkMobileMenu();
-						}
-							if (id == ('aboutme-link') || id ==('mobile-link-aboutme') || id ==('hero-arrow-btn-mobile') || id ==('hero-arrow-btn')) {
-								section = "aboutme";
-								scrollTo(section);
-								console.log("clicked");
-								checkMobileMenu();
-							}
-								if (id == ('projects-link')) {
-									dropDownMenu.fadeToggle(200).delay( 100 ).css({display: 'grid'});
-									$(dropDownMenu).mouseleave( function() {
-										$(dropDownMenu).fadeOut(200);
-									});
-								}
-									if (id == ('mobile-menu-btn') || id == ('mobile-menu-display')) {
-										checkMobileMenu();
-									}
-				});
+    if (id == ('home') || id == ('home-link')) {
+        section = "top";
+        scrollTo(section); // Passes the section to scroll to
+    }
+    if (id == ('mobile-link-top')) {
+        section = "top";
+        scrollTo(section); // Passes the section to scroll to
+        checkMobileMenu();
+    }
+    if (id == ('dropdown-project1')) {
+        section = "project1";
+        dropDownMenu.fadeToggle(200).delay(100).css({ display: 'grid' });
+        scrollTo(section);
+    }
+    if (id == ('dropdown-project3')) {
+        section = "project3";
+        dropDownMenu.fadeToggle(200).delay(100).css({ display: 'grid' });
+        scrollTo(section);
+    }
+    if (id == ('cta-my-work') || id == ('mobile-link-projects')) {
+        section = "projects";
+        scrollTo(section);
+        checkMobileMenu();
+    }
+    if (id == ('designs-link') || id == ('mobile-link-designs')) {
+        section = "designs";
+        scrollTo(section);
+        checkMobileMenu();
+    }
+    if (id == ('aboutme-link') || id == ('mobile-link-aboutme') || id == ('hero-arrow-btn-mobile') || id == ('hero-arrow-btn')) {
+        section = "aboutme";
+        scrollTo(section);
+        console.log("clicked");
+        checkMobileMenu();
+    }
+    if (id == ('projects-link')) {
+        dropDownMenu.fadeToggle(200).delay(100).css({ display: 'grid' });
+        $(dropDownMenu).mouseleave(function() {
+            $(dropDownMenu).fadeOut(200);
+        });
+    }
+    if (id == ('mobile-menu-btn') || id == ('mobile-menu-display')) {
+        checkMobileMenu();
+    }
+});
 
 // scroll function {
-	function scrollTo(section) {
-		var section = section;
-		$([ document.documentElement, document.body ]).animate(
-			{
-				scrollTop: ($('#' + section).offset().top - 60 )
-			},
-			800
-		);
-	} 
+function scrollTo(section) {
+    var section = section;
+    $([document.documentElement, document.body]).animate({
+            scrollTop: ($('#' + section).offset().top - 60)
+        },
+        800
+    );
+}
 // Checks if mobile mobile menu
 function checkMobileMenu() {
-	let mobileMenu = $('.mobile-menu-display')
-	if (mobileMenu.css('top') !== '50px') {
-		mobileMenu.animate({top: 50 });
-		}
-		else {
-			mobileMenu.animate({top: '-100%', height: 'auto'});
+    let mobileMenu = $('.mobile-menu-display')
+    if (mobileMenu.css('top') !== '50px') {
+        mobileMenu.animate({ top: 50 });
+    } else {
+        mobileMenu.animate({ top: '-100%', height: 'auto' });
+    }
 }
-}
+// Image hover light animation
